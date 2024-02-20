b063e480e7d575f20ad6e37d44e5d7e4b89d9b99
adityaseptiann <dittsyn160@gmail.com>
2024-02-20 11:53:06 +0700

    Basic Routing Jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//basic raoute//
+
 Route::get('/hello', function () {
     return 'Hello World';
 });
@@ -28,3 +30,61 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return 'Nama: Ellois Karina Handoyo <br> Nim: 2241720154';
 });
+
+//Optional Parameter//
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/article/{id}', function ($idId) {
+    return "Halaman Artikel dengan ID ".$idId;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+// Route Name //
+
+Route::get('/user/profile', function() {
+    //
+    })->name('profile');
+
+// Route Group //
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+    // Uses first & second middleware...
+});
+Route::get('/user/profile', function () {
+// Uses first & second middleware...
+    });
+});
+Route::domain('{account}.example.com')->group(function () {
+Route::get('user/{id}', function ($account, $id) {
+//
+    });
+});
+Route::middleware('auth')->group(function () {
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/post', [PostController::class, 'index']);
+Route::get('/event', [EventController::class, 'index']);
+});
+
+//Route Prefixes//
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+    });
+
+//Redirect Routes//
+Route::redirect('/here', '/there');
+
+// view routes //
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
